9a7a2bab51fa6929c4892592deabd74d844ca4d1
aimeenhile <aimeenhile@gmail.com>
2025-10-02 19:37:16 -0700

    sh

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -28,8 +28,6 @@ echo "=========================================="
 echo "Running Codon tests..."
 echo "=========================================="
 
-codon build bio_codon -release -o bio_codon_compiled.so | { echo "Codon compilation failed."; exit 1; } 
-
 codon run bio_codon/motifs/test.py > codon_output.txt 2>&1 || { 
     echo "------------------------------------------"
     echo "CODON TEST FAILED - TRACEBACK BELOW:"
