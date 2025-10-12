02eafd283a1d7fdfffd40ffda6ea065be4301a2b
aimeenhile <aimeenhile@gmail.com>
2025-10-12 16:21:56 -0700

    update ci&sh

--- week3/evaluate.sh
+++ week3/evaluate.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 echo "=== Compiling Cython files in week3/biotite ==="
-python3 -m pip install --upgrade pip setuptools wheel cython > /dev/null 2>&1
 python -m Cython.Build.cythonize -i week3/biotite/*.pyx
 
 echo "============================="
@@ -14,6 +13,4 @@ python3 week3/test_phylo.py
 echo "============================="
 echo "🧬 Running Codon tests"
 echo "============================="
-export PATH=${HOME}/.codon/bin:$PATH
-export CODON_RUNTIME=1
 codon run week3/bio_codon/test_phylo.py
