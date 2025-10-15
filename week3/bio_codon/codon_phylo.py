566434ae5b4de76274cd3db3968b410fa7e632c1
aimeenhile <aimeenhile@gmail.com>
2025-10-15 16:16:36 -0700

    test

--- week3/bio_codon/codon_phylo.py
+++ week3/bio_codon/codon_phylo.py
@@ -35,10 +35,10 @@ class TreeError(Static[Exception]):
 
 class TreeNode:
 
-    def __init__(self,
-                 children: Optional[List["TreeNode"]] = None,
-                 distances: Optional[List[float]] = None,
-                 index: Optional[int] = None) -> None:
+    def __init__[T](self,
+                 children: Optional[list["TreeNode"]] = None,
+                 distances: Optional[list[float]] = None,
+                 index: Optional[int] = None):
         """
         If index is provided -> leaf node.
         Otherwise -> intermediate node, children and distances must be provided.
