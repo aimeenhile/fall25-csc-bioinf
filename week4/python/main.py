9341e291f14fa51d9728c3d105c6496022c80833
aimeenhile <aimeenhile@gmail.com>
2025-10-22 11:38:46 -0700

    update name

--- week4/python/main.py
+++ week4/python/main.py
@@ -39,9 +39,9 @@ if __name__ == "__main__":
         for method, align_function in methods:
             start = time.perf_counter()
             if method == "affine":
-                alignment = align_function(s1, s2, match=MATCH, mismatch=MISMATCH, gap_open=GAP_OPEN, gap_extend=GAP_EXTENSION)
+                score_val, align1, align2 = align_function(s1, s2, match=MATCH, mismatch=MISMATCH, gap_open=GAP_OPEN, gap_extend=GAP_EXTENSION)
             else:
-                alignment = align_function(s1, s2, match=MATCH, mismatch=MISMATCH, gap=GAP)
+                score_val, align1, align2  = align_function(s1, s2, match=MATCH, mismatch=MISMATCH, gap=GAP)
             end = time.perf_counter()
             runtime_ms = (end - start) * 1000  
             
