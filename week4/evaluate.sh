8bc24d44220973ca4946a3ca4921922dfbae9e21
aimeenhile <aimeenhile@gmail.com>
2025-10-22 01:37:52 -0700

    update dir2

--- week4/evaluate.sh
+++ week4/evaluate.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 cd week4
 
 # Path to data folder
-DATA_DIR="./week4/data"
+DATA_DIR="./data"
 
 # Run Python version
 echo -e "Method\tLanguage\tRuntime"
 echo "--------------------------------------"
-python /python/main.py "$DATA_DIR" | while read -r line; do
+python python/main.py "$DATA_DIR" | while read -r line; do
     echo -e "$line"
 done
 
