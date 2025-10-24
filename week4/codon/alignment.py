a5d50b3f278e296aa92fa35e8a06060c03fd5c7e
aimeenhile <aimeenhile@gmail.com>
2025-10-24 08:36:11 -0700

    test

--- week4/codon/alignment.py
+++ week4/codon/alignment.py
@@ -13,8 +13,10 @@ def global_alignment(s1: str, s2: str, match: int = MATCH, mismatch: int = MISMA
     n: int = len(s1)
     m: int = len(s2)
 
-    D_prev: np.ndarray[int, 1] = np.ndarray([m + 1], dtype=int, ndim=1)
-    D_prev.fill(0)
+    data_prev = [0 for _ in range(m + 1)]        # initialize a Python list
+    D_prev: np.ndarray[int, 1] = np.ndarray((m,), data_prev) 
+    #D_prev: np.ndarray[int, 1] = np.ndarray([m + 1], dtype=int, ndim=1)
+    #D_prev.fill(0)
 
     D_curr: np.ndarray[int, 1] = np.ndarray([m + 1], dtype=int, ndim=1)
     D_curr.fill(0)
