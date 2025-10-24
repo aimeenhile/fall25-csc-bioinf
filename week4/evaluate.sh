316d569265ea27625b8d8881fd9d9eee8934a564
aimeenhile <aimeenhile@gmail.com>
2025-10-24 00:28:55 -0700

    update print

--- week4/evaluate.sh
+++ week4/evaluate.sh
@@ -7,7 +7,7 @@ cd week4
 # Run Python version
 echo -e "Method\t\tLanguage\tRuntime"
 echo "--------------------------------------"
-python python/main.py data >out.txt | while read -r line; do
+python python/main.py data | while read -r line; do
     echo -e "$line"
 done
 #codon run -release codon/main.py data >out.txt | while read -r line; do
