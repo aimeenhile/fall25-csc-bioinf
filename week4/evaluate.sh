3d8a4b856109996de2e0d27cabac00173071b0d0
aimeenhile <aimeenhile@gmail.com>
2025-10-24 18:15:24 -0700

    test

--- week4/evaluate.sh
+++ week4/evaluate.sh
@@ -5,11 +5,18 @@ set -euo pipefail
 cd week4
 
 # Run Python version
-echo f"{'Method':<25}{'Language':<12}{'Runtime'}"
-echo "-------------------------------------------------"
+printf "%-25s%-12s%s\n" "Method" "Language" "Runtime"
+printf "%-25s%-12s%s\n" "-------------------------" "------------" "-------"
+
 codon run -release codon/main.py data | while read -r line; do
-    echo -e "$line"
+    method=$(echo "$line" | cut -f1)
+    language=$(echo "$line" | cut -f2)
+    runtime=$(echo "$line" | cut -f3)
+    printf "%-25s%-12s%s\n" "$method" "$language" "$runtime"
 done
 python python/main.py data | while read -r line; do
-    echo -e "$line"
+    method=$(echo "$line" | cut -f1)
+    language=$(echo "$line" | cut -f2)
+    runtime=$(echo "$line" | cut -f3)
+    printf "%-25s%-12s%s\n" "$method" "$language" "$runtime"
 done
