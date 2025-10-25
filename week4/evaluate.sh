7ab74a2cb43e23ec3ab32bad4241f91d804a9d71
aimeenhile <aimeenhile@gmail.com>
2025-10-24 18:19:21 -0700

    format ci table

--- week4/evaluate.sh
+++ week4/evaluate.sh
@@ -8,15 +8,6 @@ cd week4
 printf "%-25s%-12s%s\n" "Method" "Language" "Runtime"
 printf "%-25s%-12s%s\n" "-------------------------" "------------" "-------"
 
-codon run -release codon/main.py data | while read -r line; do
-    method=$(echo "$line" | cut -f1)
-    language=$(echo "$line" | cut -f2)
-    runtime=$(echo "$line" | cut -f3)
-    printf "%-25s%-12s%s\n" "$method" "$language" "$runtime"
-done
-python python/main.py data | while read -r line; do
-    method=$(echo "$line" | cut -f1)
-    language=$(echo "$line" | cut -f2)
-    runtime=$(echo "$line" | cut -f3)
-    printf "%-25s%-12s%s\n" "$method" "$language" "$runtime"
-done
+codon run -release codon/main.py data 
+
+python python/main.py data 
