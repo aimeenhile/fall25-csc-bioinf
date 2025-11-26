9954ca6d606ca2802abc2063cf692a1965a6e3ea
aimeenhile <aimeenhile@gmail.com>
2025-11-26 09:00:28 -0800

    imp

--- final-project/src/exon_inclusion/functions.py
+++ final-project/src/exon_inclusion/functions.py
@@ -3,13 +3,13 @@
 from __future__ import annotations
 import pandas as pd
 import numpy as np
-import scipy as sp
+from scipy.stats import spearmanr
 from pathlib import Path
 from typing import Tuple, Dict
 
 REGIONS = ["VIS", "HIPP"]
 
-def load_type_neurons(path: str | Path):
+def load_type_neurons(path: str | Path) -> pd.DataFrame:
     """Load altExonUsage_devel_type.gz and preprocess fields"""
 
     path = Path(path)
@@ -19,12 +19,18 @@ def load_type_neurons(path: str | Path):
     neurons = [col for col in df.columns if "Neuron" in col]
     type_neurons = df[["Exon", "Gene"] + neurons].copy()
 
-    # Combine Exon + Gene into GE
+    # Combine Exon + Gene into GE (Exon::Gene)
     type_neurons["GE"] = type_neurons["Exon"] + "::" + type_neurons["Gene"]
     type_neurons = type_neurons.set_index("GE")
     type_neurons = type_neurons.drop(columns=["Exon", "Gene"])
 
     return type_neurons
 
-def get_correlation():
-    """ """
+def compute_spearman_corr(df: pd.DataFrame) -> pd.DataFrame:
+    """Compute Spearman correlation matrix"""
+
+    corr, _ = spearmanr(df, axis=0, nan_policy='omit')
+    CN = pd.DataFrame(corr, index=df.columns, columns=df.columns)
+
+    return CN
+
