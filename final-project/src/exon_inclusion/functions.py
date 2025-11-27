e4db7cb0e8cb9fafd98db3a38aafb2562828b3cc
aimeenhile <aimeenhile@gmail.com>
2025-11-27 00:02:57 -0800

    fig 4a

--- final-project/src/exon_inclusion/functions.py
+++ final-project/src/exon_inclusion/functions.py
@@ -26,9 +26,10 @@ def load_type_neurons(path: str | Path) -> pd.DataFrame:
 
 def compute_spearman_corr(df: pd.DataFrame) -> pd.DataFrame:
     """Compute Spearman correlation matrix & Clean up row and column names"""
-
-    corr, _ = spearmanr(df, axis=0, nan_policy='omit')
-    CN = pd.DataFrame(corr, index=df.columns, columns=df.columns)
+    
+    df_mod= df.dropna(axis=0, how='any')
+    corr, p = spearmanr(df_mod, axis=0, nan_policy='omit')
+    CN = pd.DataFrame(corr, index=df_mod.columns, columns=df_mod.columns)
 
     # Rename "Hippocampus" -> "HIPP" & "VisCortex" -> "VIS"
     rename = (CN.index
@@ -48,8 +49,4 @@ def spearman_corr_labels(CN: pd.DataFrame) -> pd.DataFrame:
     labels[["Age", "Region", "Type"]] = labels["CT"].str.split("_", expand=True)
 
     return labels
-
-def sort_excite_inhib(labels: pd.DataFrame):
-    return 0
-
 
